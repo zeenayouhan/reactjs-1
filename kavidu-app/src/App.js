d2c7610db927ba275f66334d4fafb3292bb43ea6
zeenayouhan <zinayouhan33@gmail.com>
2019-11-12 16:24:14 +0530

    second

--- kavidu-app/src/App.js
+++ kavidu-app/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Greet';
+import Welcome from './component/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <Greet></Greet>
+      <Greet></Greet>
+      <Welcome></Welcome>
           </div>
   );
 }

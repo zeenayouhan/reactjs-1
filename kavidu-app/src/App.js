fa6d3c1dd4d46340fa757c0831c84f234ba3ed24
zeenayouhan <zinayouhan33@gmail.com>
2019-11-12 12:36:29 +0530

    initial

---
+++ kavidu-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        hello world kavindu
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
